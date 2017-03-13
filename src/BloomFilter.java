276a056681bb29fb6786b178773cdbe8260233eb
chmodzxz <chmodzxz@gmail.com>
2017-03-13 20:18:53 +0800

    test git

---
+++ src/BloomFilter.java
@@ -0,0 +1,17 @@
+/**
+ * Created by zxz on 17/3/13.
+ */
+public class BloomFilter {
+
+    public BloomFilter(){
+
+    }
+
+    public void insert(Object key){
+
+    }
+
+    public boolean exists(Object key){
+
+    }
+}
